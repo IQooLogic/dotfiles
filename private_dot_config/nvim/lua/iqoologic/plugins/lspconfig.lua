d9014c66ddb959a5b804c3b6288bff4e2bacb93c
milos <iqoologic@gmail.com>
2026-07-23 23:10:01 +0200

    chore: mason should install markdownlint

--- private_dot_config/nvim/lua/iqoologic/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/iqoologic/plugins/lspconfig.lua
@@ -267,8 +267,9 @@ return {
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"stylua",
-				"gopls", -- Used to format Lua code
+		"stylua",
+		"markdownlint",
+		"gopls",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
